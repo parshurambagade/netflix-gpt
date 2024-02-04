139a2cb0d54913720b77b6fc2cb9a51173ce93b8
Parshuram <parshurambagade855@gmail.com>
2024-02-04 23:47:08 +0530

    resolve responsive issues

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -80,11 +80,11 @@ const Login = () => {
         <div className="fixed -z-10 lg:hidden">
           <div className="bg-black h-screen w-screen bg-opacity-70"></div>
         </div>
-        <div className="my-8">
+        <div className="mt-8">
           <Header />
         </div>
-        <div className="lg:flex lg:flex-col lg:items-center  pt-12 lg:pt-28 ">
-        <form className="absolute mt-20 text-white w-full bg-transparent lg:bg-black  rounded-lg  md:w-1/2 lg:w-3/12 p-6 md:p-8 lg:p-12  lg:bg-opacity-85 mx-auto flex flex-col gap-4 lg:box-border text-sm">
+        <div className="lg:flex lg:flex-col lg:items-center  pt-6 md:pt-16 lg:pt-28 ">
+        <form className="flex mt-4 lg:mt-20 text-white w-full bg-transparent lg:bg-black  rounded-lg  md:w-1/2 lg:w-3/12 p-6 md:p-8 lg:p-12  lg:bg-opacity-85 mx-auto  md:flex flex-col gap-4 lg:box-border md:text-left  text-sm">
             <h2 className="font-bold text-2xl lg:text-3xl lg:my-4">{isSignInForm ? "Sign In" : "Sign Up"}</h2>
             {!isSignInForm && <input type="text" ref={displayName} name="name" placeholder="Full name" className="w-full bg-gray-800 p-4 border border-gray-500 rounded-md"/>}
             <input ref={email} type="email" name="email" placeholder="Email address" className="w-full bg-gray-800 p-4 border border-gray-500 rounded-md"/>
@@ -95,12 +95,12 @@ const Login = () => {
             <button type="submit" className="bg-red-700 font-bold p-4  w-full rounded-md" onClick={handleFormSubmit}>{!isSignInForm ? "Sign Up" : "Sign In"}</button>
             <p className="md:text-base md:text-center lg:text-sm">Forgot password ?</p>
 
-            <div className="md:mx-auto md:my-16 lg:my-8 lg:mx-0">
+            <div className="md:mx-auto  md:my-16 lg:my-8 lg:mx-0">
         {isSignInForm 
         ? 
-        <p className="text-white">New to Netflix? <span className="font-bold cursor-pointer" onClick={() => setIsSignInForm(!isSignInForm)}>Sign up now.</span></p>
+        <p className="text-white md:text-left">New to Netflix? <span className="font-bold cursor-pointer" onClick={() => setIsSignInForm(!isSignInForm)}>Sign up now.</span></p>
         : 
-        <p className="text-white">Already registered? <span className="font-bold cursor-pointer" onClick={() => setIsSignInForm(!isSignInForm)}>Sign in now.</span></p>}
+        <p className="text-white ">Already registered? <span className="font-bold cursor-pointer" onClick={() => setIsSignInForm(!isSignInForm)}>Sign in now.</span></p>}
         </div> 
         </form>
         </div>
