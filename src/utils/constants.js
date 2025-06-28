097def15f7efb0f859bf39c3044a0c9f4104552e
Parshuram Bagade <parshurambagade855@gmail.com>
2025-06-28 22:37:50 +0530

    fix(constants): remove environment variables from constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,20 +7,21 @@ export const NETFLIX_LOGO =
 export const PROFILE_PIC =
   "https://i.pinimg.com/originals/e3/94/30/e39430434d2b8207188f880ac66c6411.png";
 
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: import.meta.env.TMDB_API_AUTHORIZATION_KEY,
-  },
-};
+// export const API_OPTIONS = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization: import.meta.env.TMDB_API_AUTHORIZATION_KEY,
+//   },
+// };
 
 export const CORS_ORIGIN_PROXY = "https://api.allorigins.win/get?url=";
-export const TMDB_API_KEY = import.meta.env.TMDB_API_KEY;
+// export const TMDB_API_KEY = import.meta.env.TMDB_API_KEY;
 
-export const TMDB_NOW_PLAYING_MOVIES = `${CORS_ORIGIN_PROXY}${encodeURIComponent(
-  `https://api.themoviedb.org/3/movie/now_playing?page=1&api_key=${TMDB_API_KEY}`
-)}`;
+export const TMDB_NOW_PLAYING_MOVIES = `${CORS_ORIGIN_PROXY}${
+  encodeURIComponent()
+  // `https://api.themoviedb.org/3/movie/now_playing?page=1&api_key=${TMDB_API_KEY}`
+}`;
 
 export const MOVIE_VIDEOS = `https://api.themoviedb.org/3/movie/`;
 
@@ -29,19 +30,22 @@ export const TMDB_PERSON_MOVIE_CREDITS = `https://api.themoviedb.org/3/person/`;
 
 export const IMG_CDN_URL = `https://image.tmdb.org/t/p/`;
 
-export const TMDB_POPULAR_MOVIES = `${CORS_ORIGIN_PROXY}${encodeURIComponent(
-  `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${TMDB_API_KEY}`
-)}`;
+export const TMDB_POPULAR_MOVIES = `${CORS_ORIGIN_PROXY}${
+  encodeURIComponent()
+  // `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${TMDB_API_KEY}`
+}`;
 
-export const TMDB_TOP_RATED_MOVIES = `${CORS_ORIGIN_PROXY}${encodeURIComponent(
-  `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${TMDB_API_KEY}`
-)}`;
+export const TMDB_TOP_RATED_MOVIES = `${CORS_ORIGIN_PROXY}${
+  encodeURIComponent()
+  // `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${TMDB_API_KEY}`
+}`;
 
-export const TMDB_UPCOMING_MOVIES = `${CORS_ORIGIN_PROXY}${encodeURIComponent(
-  `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=${TMDB_API_KEY}`
-)}`;
+export const TMDB_UPCOMING_MOVIES = `${CORS_ORIGIN_PROXY}${
+  encodeURIComponent()
+  // `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=${TMDB_API_KEY}`
+}`;
 
-export const TMDB_GET_MOVIES_BY_KEYWORD = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&include_adult=false&language=en-US&page=1&query=`;
+// export const TMDB_GET_MOVIES_BY_KEYWORD = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&include_adult=false&language=en-US&page=1&query=`;
 
 export const TMDB_GET_MOVIE_DETAILS = `https://api.themoviedb.org/3/movie/`;
 export const TMDB_GET_PERSON_DETAILS = `https://api.themoviedb.org/3/person/`;
