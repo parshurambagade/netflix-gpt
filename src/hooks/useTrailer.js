cc0288d40e10cbf8f272d14ad4e9dfc82d5bcc6c
Parshuram Bagade <parshurambagade855@gmail.com>
2025-06-30 16:26:20 +0530

    feat(api/movie-videos): add serverless function to fetch movie videos (trailer)

--- src/hooks/useTrailer.js
+++ src/hooks/useTrailer.js
@@ -1,5 +1,4 @@
 import { useCallback, useEffect } from "react";
-import { CORS_ORIGIN_PROXY, MOVIE_VIDEOS } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addTrailer } from "../redux/moviesSlice";
 
@@ -9,24 +8,16 @@ const useTrailer = (movieId) => {
 
   const fetchMovieVideos = useCallback(async () => {
     try {
-      const data = await fetch(
-        `${CORS_ORIGIN_PROXY}${encodeURIComponent(
-          `${MOVIE_VIDEOS + movieId}/videos?api_key=${
-            import.meta.env.VITE_TMDB_API_KEY
-          }`
-        )}`
-      );
+      const data = await fetch(`/api/movie-videos?movieId=${movieId}`);
 
       const json = await data.json();
 
       //checks for the type = Trailer
-      const filteredData = await JSON.parse(json?.contents)?.results?.filter(
+      const filteredData = await json?.results?.filter(
         (video) => video.type === "Trailer"
       );
 
-      const trailer = filteredData?.length
-        ? filteredData[0]
-        : JSON.parse(json?.contents)?.results[0];
+      const trailer = filteredData?.length ? filteredData[0] : json?.results[0];
 
       dispatch(addTrailer(trailer));
     } catch (e) {
